19aeb33fc020f321b13e2134f0765513491dfd6d
Pro_coder <83695512+Premkumar9799817360@users.noreply.github.com>
2023-05-22 15:51:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 import pandas as pd
 
-# popular_df = pickle.load(open("popular.pkl", "rb"))
-popular_df = pd.read_pickle('popular.pkl')
+popular_df = pickle.load(open("popular.pkl", "rb"))
+# popular_df = pd.read_pickle('popular.pkl')
 pt = pickle.load(open("pt.pkl","rb"))
 books = pickle.load(open("books.pkl","rb"))
 similarity_scores = pickle.load(open("similarity_score.pkl","rb"))
